ee2c1b76e70f37aa9ca784303f94d24341c43dbc
SiddharthS15 <siddharthsadake@gmail.com>
2025-04-18 21:42:47 +0530

    first commit

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIR5MVoyfluEVRqzzw3OWGUwQAuBIzrGw",
-  authDomain: "finance-flow-87a44.firebaseapp.com",
-  projectId: "finance-flow-87a44",
-  storageBucket: "finance-flow-87a44.appspot.com",
-  messagingSenderId: "147339452036",
-  appId: "1:147339452036:web:28106258b779b7c59b2cd1",
-  measurementId: "G-HRSM4Y21SY"
+  apiKey: "AIzaSyBZgfL2eWo26wouHGCecGKgyZMURmm2_bY",
+  authDomain: "expense-track-9cc69.firebaseapp.com",
+  projectId: "expense-track-9cc69",
+  storageBucket: "expense-track-9cc69.firebasestorage.app",
+  messagingSenderId: "736652496734",
+  appId: "1:736652496734:web:8e466b87f8731a798649cf",
+  measurementId: "G-TT3B3ESMPL"
 };
 
 // Initialize Firebase
